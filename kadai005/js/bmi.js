697cbe542601d32386122fbe08c513dd59604b23
usuakari <xylez999@gmail.com>
2025-07-05 13:10:55 +0900

    LETに変更

--- kadai005/js/bmi.js
+++ kadai005/js/bmi.js
@@ -1,7 +1,7 @@
 // 定数の宣言・値の代入
-const weight = 68;
-const height = 1.7;
-const result = weight / (height * height)
+let weight = 68;
+let height = 1.7;
+let result = weight / (height * height)
 
 // コンソールへの出力
 console.log("BMI=",result);
